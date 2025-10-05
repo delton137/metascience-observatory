29a96371ec570199ac0ef31acf70bfcbef1badde
Daniel Elton <daniel.elton@ccds.io>
2025-10-05 11:17:43 -0400

    santiy

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-10-05'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-01'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
