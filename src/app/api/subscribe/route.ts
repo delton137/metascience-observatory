a9a625328f8756540fbdaf3ab420a35a7bd5da25
Elton, Daniel Christopher <delton@mgb.org>
2025-10-06 07:33:35 -0400

    update

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -15,6 +15,7 @@ export async function POST(request: Request) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Accept: "application/json",
         Authorization: `Token ${apiKey}`,
       },
       body: JSON.stringify({
