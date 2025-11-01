93392c32351d883570cc6f664d269591b42c07f4
Elton, Daniel Christopher <delton@mgb.org>
2025-11-01 11:42:46 -0400

    sanity studio

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,7 @@
-import type {StructureResolver} from 'sanity/structure'
+import type {StructureBuilder, StructureResolver} from 'sanity'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
+export const structure: StructureResolver = (S: StructureBuilder) =>
   S.list()
     .title('Content')
     .items(S.documentTypeListItems())
