b45c963bda96a174521f4ce570e45b3ca8e785a5
Elton, Daniel Christopher <delton@mgb.org>
2025-12-16 12:07:34 -0500

    update articles

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -12,7 +12,37 @@ const articles: ArticleMeta[] = [
   {
     href: "/replication-projects",
     title: "Previous replication initiatives",
-    date: "12-10-2025",
+    date: "2025-12-10",
+  },
+  {
+    href: "https://moreisdifferent.substack.com/p/german-scientific-paternalism-and-the-golden-age",
+    title: "The golden age of German science (1880 - 1930)",
+    date: "2025-01-28",
+  },
+  {
+    href: "https://www.asimov.press/p/peer-review",
+    title: "A Defense of Peer Review (Asimov Press)",
+    date: "2024-10-22",
+  },
+  {
+    href: "https://moreisdifferent.blog/p/wth-is-cerebrolysin-actually",
+    title: "WTH is Cerebrolysin, actually?",
+    date: "2024-08-05",
+  },
+  {
+    href: "https://moreisdifferent.substack.com/p/how-common-is-scientific-fraud",
+    title: "How common is scientific fraud?",
+    date: "2023-08-05",
+  },
+  {
+    href: "https://moreisdifferent.substack.com/p/the-deluge-of-crappy-papers-must",
+    title: "The deluge of crappy papers must stop",
+    date: "2022-02-28",
+  },
+  {
+    href: "https://moreisdifferent.substack.com/p/ai-for-covid-19-diagnosis-a-case",
+    title: "AI for COVID-19 diagnosis - a case study in bad incentives",
+    date: "2021-04-15",
   },
 ];
 
