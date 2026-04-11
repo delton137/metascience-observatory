1de6f6c32029e2bf5544951305a527e1a438d2c5
Elton, Daniel Christopher <delton17@gmail.com>
2026-04-11 10:10:05 -0400

    docs

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -42,6 +42,10 @@ const documentation: DocMeta[] = [
     title: "V6 extraction pipeline evaluation",
   },
   {
+    href: "https://explore.metascienceobservatory.org/about",
+    title: "Metascience Observatory Explorer Data Sources",
+  },
+  {
     href: "/roadmap",
     title: "Roadmap",
   },
